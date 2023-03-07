b2d3674f4e8236b1132fd55961d937517c733e3d
KaviorSalandez <caankhwh1303@gmail.com>
2023-03-07 17:22:54 +0700

    update

---
+++ SWP/src/java/admin/UpdateAccount.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package admin;
+
+import dal.DashboardDAO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import model.User;
+
+
+/**
+ *
+ * @author hp
+ */
+
+@WebServlet(name = "UpdateAccount", urlPatterns = {"/updateaccount"})
+public class UpdateAccount extends HttpServlet {
+
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+
+            out.println("<title>Servlet UpdateAccount</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet UpdateAccount at " + request.getContextPath() + "</h1>");
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        DashboardDAO d = new DashboardDAO();
+        String id_raw = request.getParameter("id");
+        int id = Integer.parseInt(id_raw);
+        request.setAttribute("userInfo", d.getUserById(id));
+        
+        request.getRequestDispatcher("dashboard/updateaccount.jsp").forward(request, response);
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+                String email = request.getParameter("email");
+        String phonenum = request.getParameter("phonenum");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+//        String role_id_raw = request.getParameter("role");
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        int role_id = Integer.parseInt(request.getParameter("roleid"));
+        String id_raw = request.getParameter("id");
+//        int id = Integer.parseInt(id_raw);
+       
+        DashboardDAO d = new DashboardDAO();
+        try {
+            
+            User a = new User(role_id, firstname, lastname, username, password, email, phonenum, null, null, 0);
+            d.updateAccount(a);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+         response.sendRedirect("dashboard");
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ SWP/test/model/ProductTest.java
@@ -0,0 +1,147 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class ProductTest {
+    
+    public ProductTest() {
+    }
+
+    @Test
+    public void testGetOrderdetail() {
+    }
+
+    @Test
+    public void testSetOrderdetail() {
+    }
+
+    @Test
+    public void testGetGender() {
+    }
+
+    @Test
+    public void testSetGender() {
+    }
+
+    @Test
+    public void testGetGallery() {
+    }
+
+    @Test
+    public void testSetGallery() {
+    }
+
+    @Test
+    public void testGetCategory() {
+    }
+
+    @Test
+    public void testSetCategory() {
+    }
+
+    @Test
+    public void testGetSizeproduct() {
+    }
+
+    @Test
+    public void testSetSizeproduct() {
+    }
+
+    @Test
+    public void testGetSize() {
+    }
+
+    @Test
+    public void testSetSize() {
+    }
+
+    @Test
+    public void testGetDiscount() {
+    }
+
+    @Test
+    public void testSetDiscount() {
+    }
+
+    @Test
+    public void testGetId() {
+    }
+
+    @Test
+    public void testSetId() {
+    }
+
+    @Test
+    public void testGetCategory_id() {
+    }
+
+    @Test
+    public void testSetCategory_id() {
+    }
+
+    @Test
+    public void testGetTitle() {
+    }
+
+    @Test
+    public void testSetTitle() {
+    }
+
+    @Test
+    public void testGetGender_id() {
+    }
+
+    @Test
+    public void testSetGender_id() {
+    }
+
+    @Test
+    public void testGetDiscount_id() {
+    }
+
+    @Test
+    public void testSetDiscount_id() {
+    }
+
+    @Test
+    public void testGetThumbnail() {
+    }
+
+    @Test
+    public void testSetThumbnail() {
+    }
+
+    @Test
+    public void testGetDescription() {
+    }
+
+    @Test
+    public void testSetDescription() {
+    }
+
+    @Test
+    public void testGetCreated_at() {
+    }
+
+    @Test
+    public void testSetCreated_at() {
+    }
+
+    @Test
+    public void testGetUpdated_at() {
+    }
+
+    @Test
+    public void testSetUpdated_at() {
+    }
+    
+}
