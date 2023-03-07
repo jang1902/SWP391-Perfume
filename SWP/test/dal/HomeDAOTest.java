234e4b59353c28b68002d280522b902f0d6decdd
Polinh312 <linhmphe163922@fpt.edu.vn>
2023-03-07 17:13:55 +0700

    update

---
+++ SWP/test/dal/HomeDAOTest.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package dal;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author hp
+ */
+public class HomeDAOTest {
+    
+    public HomeDAOTest() {
+    }
+
+    @Test
+    public void testGetAllProduct() {
+    }
+
+    @Test
+    public void testGetProductNew() {
+    }
+
+    @Test
+    public void testGetProductDiscount() {
+    }
+
+    @Test
+    public void testGetProductBestSeller() {
+    }
+
+    @Test
+    public void testGetAllCategory() {
+    }
+    
+}
